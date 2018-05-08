a5321dbdcf6dc0f8637e18bbde2ebac5fc8d72d4
lens1199 <lens1199@gmail.com>
2018-05-08 21:37:10 +1000

    variable typo fixed

--- app/loadData.py
+++ app/loadData.py
@@ -1,22 +1,24 @@
-from app.models import University,Rank,Enrollments,Research_income,HDR_completions
+from app.models import University, Rank, Enrollments, Research_income, HDR_completions
 from app import db
 import pandas as pd
 import re
 import json
 
+
 def process_university_name(name):
-    name = re.sub(r'^(The|the)','',name)
+    name = re.sub(r'^(The|the)', '', name)
     name = name.strip()
-    name = re.sub(r'\n',' ',name)
-    name = re.sub(r'[ ]+',' ',name)
+    name = re.sub(r'\n', ' ', name)
+    name = re.sub(r'[ ]+', ' ', name)
     if 'RMIT' in name:
         name = 'Royal Melbourne Institute of Technology'
     if 'Federation' in name:
         name = 'Federation University Australia'
     return name
 
+
 def load_cwur_data(file_path):
-    df = pd.read_csv(file_path,encoding = "ISO-8859-1")
+    df = pd.read_csv(file_path, encoding="ISO-8859-1")
     audf = df.loc[df['country'] == 'Australia']
     audf = audf.loc[:, audf.columns != 'country']
     cwur_data = []
@@ -29,41 +31,40 @@ def load_cwur_data(file_path):
     return cwur_data
 
 
-
 def load_research_data(file_path):
     sub_header = [[1, 5, 'Australian Competitve Grants'],
                   [5, 9, 'Other Public Sector Research Funding'],
-                  [9,17, 'Industry and Other Funding for Research'],
-                  [17,21, 'Cooperative Research Center Funding']]
+                  [9, 17, 'Industry and Other Funding for Research'],
+                  [17, 21, 'Cooperative Research Center Funding']]
     research_data = []
-    for sheet,year in [(1,2016),(2,2015)]:
-        df = pd.read_excel(file_path, sheet, skiprows=6,skip_footer=2, index_col=0)
+    for sheet, year in [(1, 2016), (2, 2015)]:
+        df = pd.read_excel(file_path, sheet, skiprows=6, skip_footer=2, index_col=0)
         df = df.rename(columns=lambda x: x.strip())
         df = df.rename(columns=lambda x: re.sub(r'\n', ' ', x))
         df = df.rename(columns=lambda x: re.sub(r'[ ]+', ' ', x))
-        avaliable_institution = df.index.tolist()
-        formated_institution = [process_university_name(i) for i in avaliable_institution]
-        for institution in avaliable_institution:
+        available_institution = df.index.tolist()
+        formatted_institution = [process_university_name(i) for i in available_institution]
+        for institution in available_institution:
             temp_inst_data = {}
             df_inst = df.loc[institution]
-            for i,j,header_name in sub_header:
+            for i, j, header_name in sub_header:
                 temp_inst_data[header_name] = df_inst.iloc[i:j].to_json()
             temp_inst_data['Grand Total'] = int(df_inst.iloc[21])
             temp_inst_data['year'] = int(year)
             temp_inst_data['institution'] = institution
-            research_data.append(temp_inst_data)       
-    return formated_institution, research_data
+            research_data.append(temp_inst_data)
+    return formatted_institution, research_data
 
 
 def load_HDR_data(file_path):
     HDR_data = []
-    for sheet,year in [(3,2016),(4,2015)]:
-        df = pd.read_excel(file_path, sheet, skiprows=4, skip_footer=2,index_col=0)
+    for sheet, year in [(3, 2016), (4, 2015)]:
+        df = pd.read_excel(file_path, sheet, skiprows=4, skip_footer=2, index_col=0)
         df = df.rename(columns=lambda x: x.strip())
         df = df.rename(columns=lambda x: re.sub(r'\n', ' ', x))
         df = df.rename(columns=lambda x: re.sub(r'[ ]+', ' ', x))
         dicted_data = df.iloc[:, 1:].to_dict('index')
-        for key,value in dicted_data.items():
+        for key, value in dicted_data.items():
             value['institution'] = key
             value['year'] = year
             HDR_data.append(value)
@@ -72,15 +73,15 @@ def load_HDR_data(file_path):
 
 def load_enrollments_data(file_path):
     enrollments_data = []
-    df = pd.read_excel(file_path, 10, skiprows=4, skip_footer=5,index_col=0)
+    df = pd.read_excel(file_path, 10, skiprows=4, skip_footer=5, index_col=0)
     df = df.dropna(how='any')
-    years = list(range(2010,2018))
-    years_pair = [[year,f'{year}.1',f'{year}.2'] for year in years]
+    years = list(range(2010, 2018))
+    years_pair = [[year, f'{year}.1', f'{year}.2'] for year in years]
     for year_pair in years_pair:
         dicted_data = df[year_pair].to_dict('index')
-        for key,value in dicted_data.items():
+        for key, value in dicted_data.items():
             temp = {}
-            for sub_key,sub_value in value.items():
+            for sub_key, sub_value in value.items():
                 if sub_key == year_pair[0]:
                     temp['Applications'] = sub_value
                 elif sub_key == year_pair[1]:
@@ -92,11 +93,12 @@ def load_enrollments_data(file_path):
             enrollments_data.append(temp)
     return enrollments_data
 
-def insert_data(avaliable_institution,reserch_data,HDR_data,cwur_data,enrollments_data):
-    for institution in avaliable_institution:
+
+def insert_data(available_institution, research_data, HDR_data, cwur_data, enrollments_data):
+    for institution in available_institution:
         u = University(name=institution)
         db.session.add(u)
-    for item in reserch_data:
+    for item in research_data:
         u = University.query.filter(University.name.contains(process_university_name(item['institution']))).first()
         if u:
             ri = Research_income(
@@ -106,7 +108,7 @@ def insert_data(avaliable_institution,reserch_data,HDR_data,cwur_data,enrollment
                 cooperative_research_center_funding=item['Cooperative Research Center Funding'],
                 grand_total=item['Grand Total'],
                 year=item['year']
-                )
+            )
             u.research_income.append(ri)
         else:
             print(f"missing university:{item['institution']}")
@@ -124,10 +126,12 @@ def insert_data(avaliable_institution,reserch_data,HDR_data,cwur_data,enrollment
                 research_doctorate_hc_indigenous=item['Research Doctorate High Cost Indigenous'],
                 research_doctorate_lc_indigenous=item['Research Doctorate Low Cost Indigenous'],
                 total_indigenou_unweighted=item['Total Indigenous (Unweighted)'],
-                grand_total_non_indigenous_and_indigenous_unweighted=item['Grand Total Non-Indigenous and Indigenous (Unweighted)'],
-                grand_total_non_indigenous_and_indigenous_weighted=item['Grand Total Non-Indigenous and Indigenous (Weighted)'],
+                grand_total_non_indigenous_and_indigenous_unweighted=item[
+                    'Grand Total Non-Indigenous and Indigenous (Unweighted)'],
+                grand_total_non_indigenous_and_indigenous_weighted=item[
+                    'Grand Total Non-Indigenous and Indigenous (Weighted)'],
                 year=item['year']
-                )
+            )
             u.HDR_completions.append(hdr)
         else:
             print(f"missing university:{item['institution']}")
@@ -147,7 +151,7 @@ def insert_data(avaliable_institution,reserch_data,HDR_data,cwur_data,enrollment
                 patents=item['patents'],
                 score=item['score'],
                 year=item['year']
-                )
+            )
             u.rank.append(rank)
         else:
             print(f"missing university:{item['institution']}")
@@ -159,7 +163,7 @@ def insert_data(avaliable_institution,reserch_data,HDR_data,cwur_data,enrollment
                 offers=item['Offers'],
                 offer_rates=item['Offer rates'],
                 year=item['year']
-                )
+            )
             u.enrollments.append(enrol)
         else:
             print(f"missing university:{item['institution']}")

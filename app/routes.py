31b9d2013de07354e809b101a64812dba68ed000
lens1199 <lens1199@gmail.com>
2018-05-26 16:56:55 +1000

    modify structure, add mash up

--- app/routes.py
+++ app/routes.py
@@ -9,18 +9,18 @@ import json
 
 
 @app.route('/', methods=['GET'])
-def index():
-    return redirect(url_for('raw_data'))
+def overall():
+    return render_template('overall.html', style={'overall': 'active', 'raw': '', 'analysis': ''})
 
 
 @app.route('/raw', methods=['GET'])
 def raw_data():
-    return render_template('raw.html', style={'raw': 'active', 'analysis': ''})
+    return render_template('raw.html', style={'overall': '','raw': 'active', 'analysis': ''})
 
 
 @app.route('/analysis', methods=['GET'])
 def analysis():
-    return render_template('analysis.html', style={'raw': '', 'analysis': 'active'})
+    return render_template('analysis.html', style={'overall': '','raw': '', 'analysis': 'active'})
 
 
 @app.route('/mapTest', methods=['GET'])
